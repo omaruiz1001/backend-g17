f6f5d55b63d1354a903ca4e9c091c16f27610e3d
Your Name <you@example.com>
2022-10-18 16:51:02 -0500

    Dia01

---
+++ semana01/dia1/condicionales.py
@@ -0,0 +1,17 @@
+#ENTRADA
+numero1 = input("Numero 1 :")
+numero2 = input("Numero 2 :")
+operacion = input("Operacion a ejecutar(suma,resta) : ")
+#PROCESO
+if(operacion =="suma") :
+    resultado = int(numero1) + int(numero2)
+elif(operacion == "resta"):
+    resultado = int(numero1) - int(numero2)
+else :
+    resultado = ""
+#SALIDA
+if(resultado == ""):
+    print("Debe ingresar una operacion valida")
+else
+    print("El resultado es : " + str(resultado))
+print("adios!!!")
